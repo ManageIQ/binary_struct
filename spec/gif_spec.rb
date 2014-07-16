38cd9aa74311b0eb7b449ce6e4ddaee96bcd1cc6
Jerry Keselman <jerryk@redhat.com>
2014-07-16 10:57:22 -0400

    Add Endian Support and Fix Encoding Issues in Spec
    1. Add support for the big and little endian modifier characters
    '>' and '<', respectively.  Add the endian_spec.rb to test this
    new support.
    2. Fix binary_struct_spec.rb and gif_spec.rb tests that have been
    failing since the ruby upgrade from 1.8.7 to 1.9.3 due to issues
    with the binary encoding of strings.

--- spec/gif_spec.rb
+++ spec/gif_spec.rb
@@ -16,23 +16,24 @@ describe BinaryStruct do
   end
 
   it "read a gif header" do
-    filename = File.join(File.dirname(__FILE__), %w{data test.gif})
-
-    header_size = gif_header.size
-    header = File.open(filename, "rb") { |f| f.read(header_size) }
-    gif_header.decode(header).should == {
+    filename = File.join(File.dirname(__FILE__), %w(data test.gif))
+    decoded_header_hash = {
       :magic              => "GIF",
       :version            => "89a",
       :width              => 16,
       :height             => 16,
       :flags              => "\x80",
       :bg_color_index     => 0,
-      :pixel_aspect_ratio => 0
+      :pixel_aspect_ratio => 0,
     }
+    encoded_header = gif_header.encode(decoded_header_hash)
+    header_size = gif_header.size
+    header = File.open(filename, "rb") { |f| f.read(header_size) }
+    encoded_header.should == header
   end
 
   it "write a gif header" do
-    header = gif_header.encode({
+    header = gif_header.encode(
       :magic              => "GIF",
       :version            => "89a",
       :width              => 16,
@@ -40,8 +41,8 @@ describe BinaryStruct do
       :flags              => "\x80",
       :bg_color_index     => 0,
       :pixel_aspect_ratio => 0
-    })
+    )
 
-    header.should == "GIF89a\x10\x00\x10\x00\x80\x00\x00"
+    header.should == "GIF89a\x10\x00\x10\x00\x80\x00\x00".force_encoding("ASCII-8BIT")
   end
 end
